ab6b98799ba54ba81c6120bb0c1e1e5924e5b458
Kirill Shestakov <20163717+guitarino@users.noreply.github.com>
2020-05-20 23:41:00 -0700

    making the dot interactive

--- src/components/AnimatedPlayground.tsx
+++ src/components/AnimatedPlayground.tsx
@@ -1,11 +1,23 @@
-import { h } from 'preact';
+import { h, Fragment } from 'preact';
 import { connect } from "@view/connect";
 import "@styles/AnimatedPlayground.scss";
+import { useState } from '@view/useState';
 
 function AnimatedPlayground() {
+	const value = useState(() => ({
+		x: 0,
+		y: 0,
+	}))
+
 	return (
-		<div className='AnimatedPlayground'>
-			<div className='AnimatedPlayground__animatedDot'></div>
+		<div className='AnimatedPlayground' onClick={e => {
+			var rect = e.currentTarget.getBoundingClientRect();
+			value.x = e.clientX - rect.left;
+			value.y = e.clientY - rect.top;
+		}}>
+			<div className='AnimatedPlayground__animatedDot' style={{
+				transform: `translateX(-50%) translateY(-50%) translateX(${value.x}px) translateY(${value.y}px)`
+			}}></div>
 		</div>
 	);
 }
