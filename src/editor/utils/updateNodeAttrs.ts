e89867b48f5139c4cf026b656ceba540e681882f
Kirill Shestakov <20163717+guitarino@users.noreply.github.com>
2020-06-02 06:25:17 -0700

    fixing node update

--- src/editor/utils/updateNodeAttrs.ts
+++ src/editor/utils/updateNodeAttrs.ts
@@ -1,6 +1,5 @@
-import { NodeType } from "prosemirror-model";
+import { NodeType, Node } from "prosemirror-model";
 import { EditorState, Transaction } from "prosemirror-state";
-import { getNodeOfType } from "@editor/utils/getNodeOfType";
 
 type Attrs = { [k: string]: any };
 
@@ -8,16 +7,21 @@ type SetAttrs = (currentAttrs: Attrs) => Attrs;
 
 export function updateNodeAttrs(pos: number, type: NodeType, setAttrs: SetAttrs) {
 	return (state: EditorState, dispatch: (t: Transaction) => any) => {
+		const nodes: Array<{ node: Node, pos: number }> = [];
 		state.doc.nodesBetween(pos, pos, (node, pos) => {
 			if (node.type === type) {
-				dispatch(
-					state.tr.setNodeMarkup(pos, type, {
-						...node.attrs,
-						...setAttrs(node.attrs)
-					})
-					.scrollIntoView()
-				);
+				nodes.push({ node, pos });
 			}
 		});
+		if (nodes.length) {
+			const node = nodes[nodes.length - 1];
+			dispatch(
+				state.tr.setNodeMarkup(node.pos, type, {
+					...node.node.attrs,
+					...setAttrs(node.node.attrs)
+				})
+				.scrollIntoView()
+			);
+		}
 	};
 }
