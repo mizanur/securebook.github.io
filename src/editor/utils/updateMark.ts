0f98edae7b423b54a2159a17ecc092a0e02b0090
Kirill Shestakov <20163717+guitarino@users.noreply.github.com>
2020-06-05 16:29:05 -0700

    improving updateMark

--- src/editor/utils/updateMark.ts
+++ src/editor/utils/updateMark.ts
@@ -1,31 +1,50 @@
-import { getMarkRange } from "@editor/utils/getMarkRange";
 import { MarkType } from "prosemirror-model";
 import { EditorState } from "prosemirror-state";
 import { Dispatch } from "@editor/interfaces/Actions";
 
+function markApplies(doc, ranges, type) {
+	for (let i = 0; i < ranges.length; i++) {
+		let {$from, $to} = ranges[i]
+		let can = $from.depth == 0 ? doc.type.allowsMarkType(type) : false
+		doc.nodesBetween($from.pos, $to.pos, node => {
+			if (can) return false
+			can = node.inlineContent && node.type.allowsMarkType(type)
+		})
+		if (can) return true
+	}
+	return false
+}
+
 export function updateMark(type: MarkType, attrs: { [k: string]: any }) {
-	return (state: EditorState, dispatch: Dispatch) => {
-		const { tr, selection, doc } = state;
-		let { from, to } = selection;
-		const { $from, empty } = selection;
-
-		if (empty) {
-			const range = getMarkRange($from, type);
-
-			if (range) {
-				from = range.from;
-				to = range.to;
+	return (state: EditorState, dispatch?: Dispatch) => {
+		let {empty, ranges} = state.selection;
+		let { $cursor } = state.selection as any;
+		if ((empty && !$cursor) || !markApplies(state.doc, ranges, type)) {
+			return false;
+		}
+		if (dispatch) {
+			if ($cursor) {
+				if (type.isInSet(state.storedMarks || $cursor.marks())) {
+					dispatch(state.tr.removeStoredMark(type));
+				}
+				dispatch(state.tr.addStoredMark(type.create(attrs)));
+			}
+			else {
+				let has = false, tr = state.tr;
+				for (let i = 0; !has && i < ranges.length; i++) {
+					let {$from, $to} = ranges[i];
+					has = state.doc.rangeHasMark($from.pos, $to.pos, type);
+				}
+				for (let i = 0; i < ranges.length; i++) {
+					let {$from, $to} = ranges[i];
+					if (has) {
+						tr.removeMark($from.pos, $to.pos, type);
+					}
+					tr.addMark($from.pos, $to.pos, type.create(attrs));
+				}
+				dispatch(tr.scrollIntoView());
 			}
 		}
-
-		const hasMark = doc.rangeHasMark(from, to, type);
-
-		if (hasMark) {
-			tr.removeMark(from, to, type);
-		}
-
-		tr.addMark(from, to, type.create(attrs));
-
-		return dispatch(tr);
+		return true;
 	}
 }
