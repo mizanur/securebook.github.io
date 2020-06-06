9b08072b6171d0464f9a1f618a5894ee3195704c
Kirill Shestakov <20163717+guitarino@users.noreply.github.com>
2020-06-05 21:32:23 -0700

    deleting unused editor menu interface

--- src/editor/interfaces/EditorMenu.ts
+++
@@ -1,7 +0,0 @@
-import { MenuState } from "@editor/interfaces/MenuState";
-import { MenuActions } from "@editor/interfaces/MenuActions";
-
-export interface EditorMenu {
-	state: MenuState,
-	actions: MenuActions
-}
